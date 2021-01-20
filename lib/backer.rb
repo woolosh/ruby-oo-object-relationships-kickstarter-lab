35165045619c5ecf56458f59143957665be8c600
woolosh <joshuapmorales@gmail.com>
2021-01-20 14:22:43 -0600

    updated

---
+++ lib/backer.rb
@@ -0,0 +1,13 @@
+class Backer
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+end
